385b74a392f8454c68bd3cacf21d64a2a74a9988
Robert <robertpanerio@Roberts-MacBook-Pro.local>
2023-02-06 23:58:17 -0800

    not so working my-sed

--- my-sed.c
+++ my-sed.c
@@ -3,20 +3,72 @@
 #include<stdio.h>
 
 int my_sed(char *buffer, char *find_term, char *replace_term){
-    // printf("%s",buffer);
-    // printf("%s\n",find_term);
-    // printf("%s\n",replace_term);
     
-    char *new_buffer = NULL;
+    
+    //replaces the newline at the end of the buffer string with a null character
+    char new_buffer[256];
     strcpy(new_buffer, buffer);
-    // int x = 0;
-    
-    while(strlen(new_buffer) >0 ){
-        char *ptr_str = strstr(new_buffer, find_term );
-        printf("%s", new_buffer);
+    new_buffer[strlen(new_buffer) - 1] = '\0';
 
-        strcpy(new_buffer, ptr_str);
+    char *ptr_str = strstr(new_buffer, find_term );
+
+    int x = 0;
+    while(x != 2){
+        if (ptr_str == NULL){
+            printf("Cannot find word\n");
+            exit(1);
+        }
+        // if word exist at the very beginning
+        if (strlen(new_buffer) == strlen(ptr_str)){
+            int len_find = strlen(find_term);
+            char *is_space = ptr_str + len_find;
+
+            // printf(">>%c<<<<space?\n", is_space[0]);
+         
+            if ( ' ' == is_space[0]){
+                char temp[256];
+
+                strcpy(temp, is_space);
+
+                int chr = 0;
+                for(chr = 0; chr < strlen(replace_term); chr++){
+                    ptr_str[chr] = replace_term[chr];
+                }
+                ptr_str[strlen(replace_term)] = '\0';
+
+                char temp2[256];
+                strcpy(temp2, ptr_str);
+                strcat(temp2, temp);
+                printf("%s\n", temp2);
+                break;
+            }
+        }
+        else{
+            // if the word is in middle 
+            // strcpy the first part the terminate
+            // strcat the replace term
+            // strcpy the end the strcat
+            char *n = ptr_str - 1;
+            printf(">>%c<<\n", n[0]);
+        }
+
+        // printf("%s\n", ptr_str);
+        ptr_str = strstr(ptr_str +1, find_term);
+        
+        x++;
     }
+
+
+
+    // printf("%s\n", new_ptr);
+
+
+    // printf("%lu\n", strlen(new_buffer));
+    // printf("%lu\n", strlen(ptr_str));
+    // printf("%lu\n", strlen(find_term));
+
+
+    
     return 0;
 }
 
