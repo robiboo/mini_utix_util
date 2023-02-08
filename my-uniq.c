ed48d05de261c8f7f1381bf3a8bc990dbd94798a
Emily Lim <emily.lim@wsu.edu>
2023-02-08 15:54:13 -0800

    Put removing duplicates code into a function. Added another test file.

--- my-uniq.c
+++ my-uniq.c
@@ -2,20 +2,51 @@
 #include<string.h>
 #include<stdio.h>
 
+// function that removes duplicates in file name specified
+int remove_dup(char *name){
+    int bytes_read = 1;
+    size_t size = 10;
+    char *string = NULL;
+    char *temp = "";
+    FILE *fp;
 
+    fp = fopen(name, "r");
+    // check if opening file was a success
+    if (fp == NULL) {
+        printf("my-uniq: cannot open file\n");
+        exit(1);
+    }
+
+    string = (char *) malloc(size);
+    while(bytes_read != EOF) {
+        bytes_read = getline(&string, &size, fp);
+        // check if lines are not duplicates
+        if (strcmp(temp, string) != 0) {
+            // free any previous temp strings
+            if (strcmp(temp, "") != 0){
+                free(temp);
+            }
+            temp = strdup(string);
+            printf("%s", string);
+        } 
+
+    }
+    free(temp);
+    free(string);
+    fclose(fp);
+
+    return 0;
+}
 
 int main(int argc, char *argv[]){
 	// if there are no files specified, take input from stdin
     FILE *fp;
 	if (argc < 2){
-		//stdin
-        int bytes_read = 1;
         size_t size = 10;
         char *string = NULL;
-        char *temp = "";
+
         // temp file to store input from stdin
         fp = fopen("temp.txt", "r+");
-
         // check if opening file was a success
         if (fp == NULL) {
             printf("my-uniq: cannot open file\n");
@@ -30,66 +61,22 @@ int main(int argc, char *argv[]){
         }
         fclose(fp);
 
-        // go through file line by line and print out non duplicates
-        fp = fopen("temp.txt", "r");
-        string = (char *) malloc(size);
-        while(bytes_read != EOF) {
-            
-            bytes_read = getline(&string, &size, fp);
-            // duplicate case 
-         
-            if (strcmp(temp, string) != 0) {
-                temp = strdup(string);
-                printf("%s", string);
-            } 
-            
-        }
-        free(temp);
-        free(string);
-        fclose(fp);
+        // call function to remove duplicates from temp file
+        remove_dup("temp.txt");
+
         remove("temp.txt");
 
-   
-
-    // If there is a file specified
+    // If there are any files specified
 	} else {
 
         int i = 1;
+        // for each input file, call remove duplicates function
         while (i < argc){
-            int bytes_read = 1;
-            size_t size = 10;
-            char *string = NULL;
-            char *temp = "";
-            FILE *fp = fopen(argv[i], "r");
-
-            // check if opening file was a success
-            if (fp == NULL) {
-                printf("my-uniq: cannot open file\n");
-                exit(1);
-            }
-
-            string = (char *) malloc(size);
-            while(bytes_read != EOF) {
-                bytes_read = getline(&string, &size, fp);
-                // duplicate case 
-                // printf("compare");
-                if (strcmp(temp, string) != 0) {
-                    if (strcmp(temp, "") != 0){
-                        free(temp);
-                    }
-                    temp = strdup(string);
-                    printf("%s", string);
-                } 
-
-            }
-            free(temp);
-            free(string);
-            fclose(fp);
+            remove_dup(argv[i]);
             i++;
-
         }
-
     
 	}
+
 	return 0;
 }
